6330dcac15ed6c20eb02cc420e01f6c1e26a38e5
mechernyshev <m.tchernyshev@gmail.com>
2023-04-26 11:49:04 +0300

    creation of user documents in firestore DB

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 
@@ -23,3 +24,30 @@ provider.setCustomParameters({
 export const auth = getAuth();
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    console.log(userDocRef)
+    const userSnapshot = await getDoc(userDocRef)
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email} = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+
+    }
+
+    return userDocRef
+}
