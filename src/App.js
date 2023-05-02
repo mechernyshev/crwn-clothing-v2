7770c5961f782c43a34f1f489665a0e25700904b
mechernyshev <m.tchernyshev@gmail.com>
2023-05-02 12:05:56 +0300

    added products context with mocked data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Directory from "./components/directory/directory.component";
 import Home from './routes/home/home.component'
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 import {Routes, Route, Outlet } from "react-router-dom";
 
-const Shop = () => {
-    return <h1>I am the shop page</h1>
-}
 
 
 
