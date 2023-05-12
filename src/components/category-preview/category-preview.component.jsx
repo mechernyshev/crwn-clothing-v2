dcad7be670b75692227901676b7b2fa25cfb8c76
mechernyshev <m.tchernyshev@gmail.com>
2023-05-12 15:41:07 +0300

    additional components styled: category preview, cart item

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,22 +1,22 @@
-import './category-preview.styles.scss'
+import {Title, Preview, CategoryPreviewContainer} from './category-preview.styles'
 import productCardComponent from "../product-card/product-card.component";
 import ProductCard from "../product-card/product-card.component";
 import {Link} from "react-router-dom";
 
 const CategoryPreview = ({title, products}) => {
     return (
-        <div className='category-preview-container'>
+        <CategoryPreviewContainer>
             <h2>
-                <Link className='title' to={title}>
+                <Title to={title}>
                     {title.toUpperCase()}
-                </Link>
+                </Title>
             </h2>
-            <div className='preview'>
+            <Preview>
                 {
                     products.filter((_, idx) => idx < 4).map((product) => <ProductCard key={product.id} product={product} /> )
                 }
-            </div>
-        </div>
+            </Preview>
+        </CategoryPreviewContainer>
     )
 }
 
